0897a0abdb93bc09e8b8ce7ad15f4f838b99e0c2
TWFILM <techapat_wong@cmu.ac.th>
2024-02-08 15:51:38 +0700

    V0.1

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -2,6 +2,7 @@ package entity;
 
 
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
@@ -13,19 +14,31 @@ public class Player extends Entity{
 	
 	GamePanel gp;
 	KeyHandler keyH;
+	
+	public final int screenX, screenY;
 	   
 	    
 	public Player(GamePanel gp, KeyHandler keyH){
 	    this.gp = gp;
 	    this.keyH = keyH;
+	    
+	    screenX = gp.screenWidth/2 - (gp.tileSize/2);
+	    screenY = gp.screenHeight/2 - (gp.tileSize/2);
+	    
+	    solidArea = new Rectangle();
+	    solidArea.x = 8;
+	    solidArea.y = 16;
+	    solidArea.width = 32;
+	    solidArea.height = 32;
 	        
 	    setDefaultValues();
 	    getPlayerImage();
 	}
 	    
 	public void setDefaultValues(){
-	    x = 100;
-	    y = 100;
+		
+	    worldX = gp.tileSize * 23;
+	    worldY = gp.tileSize * 21;
 	    speed = 4;
 	    direction = "down";
 	}
@@ -51,19 +64,19 @@ public class Player extends Entity{
 	              keyH.leftPressed == true || keyH.rightPressed == true){
 	          if(keyH.upPressed) {
 	        	  direction = "up";
-	        	  y -= speed;
+	        	  worldY -= speed;
 	          }
 	          else if (keyH.downPressed) {
 	              direction = "down";
-	              y += speed;
+	              worldY += speed;
 	           }
 	          else if (keyH.leftPressed) {
 	              direction = "left";
-	              x -= speed;
+	              worldX -= speed;
 	          }
 	          else if (keyH.rightPressed) {
 	              direction = "right";
-	              x += speed;
+	              worldX += speed;
 	          }
 
 	          spriteCounter++;
@@ -120,6 +133,6 @@ public class Player extends Entity{
 	           }
 	           break;
 	       }
-	       g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+	       g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
 	}
 }
